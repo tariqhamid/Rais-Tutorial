f0f30ba76a96ae4c88504e201a094649ecfbb61e
ryo <nanteneus@hotmail.co.jp>
2018-11-02 00:30:57 +0900

    Implement change title each pages function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def hello
-    render html: "hello, world!"
+    render html: 'hello, world!'
   end
+  
   def goodbye
-    render html: "goodbye, world!"
+    render html: 'goodbye, world!'
   end
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,25 +1,34 @@
 require 'rails_helper'
 
-RSpec.describe "StaticPages", type: :request do
-
-  describe "Home pages" do
+RSpec.describe 'Static Pages', type: :request do
+  describe 'Home pages' do
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
     end
+
+    it 'should have the base title' do
+      visit '/static_pages/home'
+      expect(page).to have_title('Ruby on Rails Tutorial Sample App')
+    end
+
+    it 'should not have acustom page title' do
+      visit '/static_pages/home'
+      expect(page).not_to have_title('|Home')
+    end
   end
 
-  describe "Help pages" do
+  describe 'Help pages' do
     it "sholud have the content 'Help'" do
       visit '/static_pages/help'
       expect(page).to have_content('Help')
     end
   end
-end
 
-describe "AboutPages" do
-  it "should have the content 'About Us'" do
-    visit '/static_pages/about'
-    expect(page).to have_content('About Us')
+  describe 'About Pages' do
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About Us')
+    end
   end
 end
