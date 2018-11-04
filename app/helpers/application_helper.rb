841445802938c0ddca72d40b31241dfaf796ab20
ryo <nanteneus@hotmail.co.jp>
2018-11-04 20:05:37 +0900

    Write Test Case of Link Click and Method 'full_title' in  ApplicatonHelper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 module ApplicationHelper
   # ページごとの完全なタイトルを返します
   def full_title(page_title)
-    base_title = "Ruby on Rails Tutorial Sample App"
+    base_title = 'Ruby on Rails Tutorial Sample App'
     if page_title.empty?
       base_title
     else
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,30 +6,58 @@ require 'support/utilities'
 RSpec.describe 'Static Pages', type: :request do
   subject { page }
 
+  shared_examples_for 'all static pages' do
+    it { should have_content(heading) }
+    it { should have_title(full_title(page_title)) }
+  end
+
   describe 'Home pages' do
     before { visit root_path }
+    let(:heading) { 'Sample App' }
+    let(:page_title) { '' }
 
-    it { should have_content('Sample App') }
-    it { should have_title(full_title('')) }
+    it_should_behave_like 'all static pages'
     it { should_not have_title('| Home') }
   end
 
   describe 'Help pages' do
     before { visit help_path }
 
-    it { should have_content('Help') }
+    let(:heading) { 'Help' }
   end
 
   describe 'About Pages' do
     before { visit about_path }
 
-    it { should have_content('About Us') }
+    let(:heading) { 'About Us' }
   end
 
   describe 'Contact Pages' do
     before { visit contact_path }
 
-    it { should have_content('Contact') }
-    it { should have_title(full_title('Contact')) }
+    let(:heading) { 'Contact' }
+    let(:page_title) { 'Contact' }
+  end
+
+  it 'should have the right links on the layout' do
+    visit root_path
+    click_link 'Sign up now!'
+    expect(page).to have_title(full_title('Sign up'))
+    click_link 'sample app'
+    expect(page).to have_title(full_title(''))
+    within('header') do
+      click_link 'About'
+      expect(page).to have_title(full_title('About Us'))
+      click_link 'Help'
+      expect(page).to have_title(full_title('Help'))
+      click_link 'Home'
+      expect(page).to have_title(full_title(''))
+    end
+    within('footer') do
+      click_link 'About'
+      expect(page).to have_title(full_title('About Us'))
+      click_link 'Contact'
+      expect(page).to have_title(full_title('Contact'))
+    end
   end
 end
